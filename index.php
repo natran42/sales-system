a74356b75813ba16ba7f74e8ab29f36749253b3a
yanelya <yanelylayala@gmail.com>
2022-03-22 16:27:18 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 
 <?php echo file_get_contents("html/header.html"); ?> 
 <?php echo file_get_contents("html/body.html"); ?> 
+
+<!-- user form  -->
+<form method="post">
+	<label for="bar">Name: </label>
+	<input type="text" name="name"><br>
+	<br><input type="submit"><br>
+
+	<?= "Name inputted: " . $_POST["name"]; ?>
+</form>
+
+
 <?php echo file_get_contents("html/footer.html"); ?>
