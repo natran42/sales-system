8256173cc70863d2d006c031391a6dd09c5b105a
yanelya <yanelylayala@gmail.com>
2022-03-22 17:04:35 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
 
 <?php include("php/main/header.php"); ?>
 <?php include("php/main/body.php"); ?> 
-
-<!-- user form  -->
-<form method="post">
-	<label for="bar">Name: </label>
-	<input type="text" name="name"><br>
-	<br><input type="submit"><br>
-
-	<?= "Name inputted: " . $_POST["name"]; ?>
-</form>
-
 <?php include("php/main/footer.php"); ?>
