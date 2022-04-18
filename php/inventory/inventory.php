586c282edb77bfb949d9ba6d358f8b647b8ebcc4
nghoang1 <57956036+nghoang1@users.noreply.github.com>
2022-04-17 21:19:16 -0500

    changed title font inventory

--- php/inventory/inventory.php
+++ php/inventory/inventory.php
@@ -186,8 +186,12 @@ if(!lowStockEmpty()) {
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
   
-  <title>Inventory</title>
+<title>Inventory</title>
   <style>
     .modal-header {
       background: #F7941E;
@@ -265,8 +269,10 @@ if(!lowStockEmpty()) {
   position: relative;
 }
 h1{
-  font-family: 'Poppins' , sans-serif;
+  font-family: 'Righteous', cursive;
+  font-size: 4em;
 }
+
   </style>
 </head>
 
